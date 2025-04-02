20f92d1b729fc563b6f281db42f958479758e899
giang5201232 <24020112@vnu.edu.vn>
2025-04-02 20:45:32 +0700

    Update Starve Feature

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,4 +1,4 @@
-﻿#include "Skeleton.h"
+#include "Skeleton.h"
 
 // Constructor: Khởi tạo skeleton với vị trí và texture
 Skeleton::Skeleton(float p_x, float p_y, SDL_Texture* p_tex) : Entity(p_x, p_y, p_tex) {
